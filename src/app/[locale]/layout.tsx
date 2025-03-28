7b465854aee1a9a7e59166adc26fed18114f4d80
Lucas Bueno <lu@voeja.us>
2025-03-28 07:25:25 -0300

    fix: types in layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,16 +1,20 @@
+import { ReactNode } from 'react';
+
 import { notFound } from 'next/navigation';
 
 import ClientLayoutWrapper from '../../components/ClientLayoutWrapper';
 
 const supportedLocales = ['pt-br', 'en', 'es', 'hi', 'ja'];
 
+interface LayoutProps {
+  children: ReactNode;
+  params: { locale: string };
+}
+
 export default async function LocaleLayout({
   children,
   params,
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
+}: LayoutProps) {
   // ⚠️ Next.js exige await para acesso de `params` em layouts com rotas dinâmicas
   // Mesmo que `params` não seja uma Promise, isso evita warnings em dev
   const locale = params.locale;
